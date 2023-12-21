72b23a0a673fbec75e0a7e87e82c83c87d2b407d
Leonardo Bilhalva <leobbbilhalva@gmail.com>
2023-12-21 09:14:28 -0300

    Initial Commit

---
+++ Relief/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Relief
+//
+//  Created by Leonardo Bilhalva on 21/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Relief/ReliefApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReliefApp.swift
+//  Relief
+//
+//  Created by Leonardo Bilhalva on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ReliefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
